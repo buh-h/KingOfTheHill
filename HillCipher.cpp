239a2f9682fd0c18cbd89ef805a1d31617beec95
buh-h <maxwellwang22@gmail.com>
2024-04-18 20:05:03 -0500

    Added basic encoding and decoding functions
    Added letter number conversion, matrix operations, hill cipher encoding and decoding

---
+++ HillCipher.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <stdexcept>
+
+// Converts a plaintext string to an queue of integers according to the A0Z25 letter number code
+std::queue<int> letterToNumber(const std::string plaintext) {
+    std::queue<int> ciphertext;
+    // Iterates through all characters in plaintext
+    for (int i=0; i<plaintext.length(); i++) {
+        // Finds the ASCII decimal value for the character after changing it to lowercase
+        int ascii = std::tolower(plaintext[i]);;
+        // Only adds letters to ciphertext, ignoring special characters and spaces
+        if (ascii - 97 <= 25 && ascii - 97 >= 0) {
+            // Adds the ASCII value-97 into ciphertext queue to convert to number with a=0,...,z=25
+            ciphertext.push(ascii - 97);
+        }
+
+    }
+    return ciphertext;
+}
+
+// Converst a ciphertext queue of integers to a string according to the A0Z25 letter number code
+std::string numberToLetter(std::queue<int> ciphertext) {
+    std::string plaintext = "";
+    // Goes through all integers in ciphertext queue
+    while (!ciphertext.empty()) {
+        // Converts int in ciphertext queue to character using ASCII values and appends it onto plaintext
+        plaintext += (char)('a' + ciphertext.front()%26);
+        // Pops the front value of ciphertext
+        ciphertext.pop();
+    }
+    return plaintext;
+}
+
+// Multiplies a matrix and a vector when vector is on right
+std::vector<int> multiplyMatrixVector(const std::vector<std::vector<int>> m, const std::vector<int> v) {
+    std::vector<int> result;
+    // Throws exception is a and b cannot be multiplied
+    if (m[0].size() != v.size()) {
+        throw std::invalid_argument("Matrix and vector cannot be multiplied!");
+    }
+
+    for (int i=0; i<m.size(); i++) {
+        result.push_back(0); 
+        // Dot product of ith row of a and b
+        for (int j=0; j<m[0].size(); j++) {
+            result[i] += m[i][j] * v[j];
+        }
+    }
+    return result;
+}
+// Multiplies a matrix and a vector when vector is on left
+std::vector<int> multiplyMatrixVector(const std::vector<int> v, const std::vector<std::vector<int>> m) {
+    std::vector<int> result;
+    // Throws exception is a and b cannot be multiplied
+    if (v.size() != m.size()) {
+        throw std::invalid_argument("Matrix and vector cannot be multiplied!");
+    }
+
+    for (int i=0; i<m[0].size(); i++) {
+        result.push_back(0); 
+        // Dot product of ith row of a and b
+        for (int j=0; j<m.size(); j++) {
+            result[i] += m[j][i] * v[j];
+        }
+    }
+    return result;
+}
+// Finds the minor of a matrix
+// Something going out of bounds or similar, its awkward
+std::vector<std::vector<int>> minor(const std::vector<std::vector<int>> m, int x, int y) {
+    std::vector<std::vector<int>> minor; 
+    int index = 0;
+    for (int i=0; i<m.size(); i++) {
+        // Creates a new row vector
+        if (i != x) {
+             minor.push_back(std::vector<int>(0, 0));
+            // Populates new row vector ignoring value when j == x
+            for (int j=0; j<m[0].size(); j++) {
+                if (j != y) {
+                    minor[0].push_back(m[i][j]);
+                }
+            }
+            index++;
+        }
+    }
+    return minor;
+}
+// Finds the determinant of a matrix
+int determinant(const std::vector<std::vector<int>> m) {
+    int detSum = 0;
+    // Base case, determinant of a 1x1 matrix is the value
+    if (m.size() == 1) {
+        return m[0][0];
+    }
+    // Calculates determinant recursively with cofactors
+    for(int i=0; i<m[0].size(); i++) {
+        detSum += m[0][i] * pow(-1, i) * determinant(minor(m, 0, i));
+    }
+    return detSum;
+}
+
+// Finds the inverse of a matrix and takes mod 26 of the result
+std::vector<std::vector<int>> invertMatrix(const std::vector<std::vector<int>> m) {
+    // Finds the adjoint of the matrix
+    std::vector<std::vector<int>> adjoint;
+    for (int i=0; i<m.size(); i++) {
+        adjoint.push_back(std::vector<int>(0, 0));
+        for (int j=0; j<m[0].size(); j++) {
+            // Swaps i and j when finding the cofactor
+            adjoint[i].push_back(determinant(minor(m, j, i)) * pow(-1, i+j));
+        }
+    }
+    // Finds the determinant of the matrix
+    int det = determinant(m);
+    int temp = pow(det, 24); // Intermediate calculation for Fermat's little theorem
+    // Finds the inverse of the matrix using the determinant and adjoint
+    std::vector<std::vector<int>> inverse;
+    for (int i=0; i<m.size(); i++) {
+        inverse.push_back(std::vector<int>(0, 0));
+        for (int j=0; j<m[0].size(); j++) {
+            // Takes mod 26 of the expression using Fermat's little therem
+            inverse[i].push_back((adjoint[i][j] % 26 * (temp % 26)) % 26);
+            // Ensures all values of inverse are positive
+            if (inverse[i][j] < 0) {
+                inverse[i][j] = inverse[i][j] + 26;
+            }
+        }
+    }
+    return inverse;
+}
+// Encodes a plaintext string with a Hill Cipher
+std::string encode(const std::string plaintext, const std::vector<std::vector<int>> encoding) {
+    std::queue<int> plaintextNumbers = letterToNumber(plaintext);
+    std::queue<int> ciphertextNumbers;
+    // Throws exception if encoding matrix is not square (nxn) 
+    if (encoding.size() != encoding[0].size()) {
+        throw std::invalid_argument("Encoding Matrix is not square!");
+    }
+    // Ensures plaintextNumbers is divisible by n by repeating the last element
+    int lastNum = plaintextNumbers.back();
+    for (int i=0; i<plaintextNumbers.size() % encoding.size(); i++) {
+        plaintextNumbers.push(lastNum);
+    }
+    // Continues until plaintextNumbers is empty
+    while (!plaintextNumbers.empty()) {
+        std::vector<int> v; // Vector with length n
+        for (int i=0; i<encoding.size(); i++) { // Repeats n times
+            // Adds the front element of ciphertext to v and removes it from plaintextNumbers
+            v.push_back(plaintextNumbers.front());
+            plaintextNumbers.pop();
+        }
+        // Multiplies v with the encoding matrix and adds the result to ciphertextNumbers
+        v = multiplyMatrixVector(encoding, v);
+        for (int & num: v) {
+            ciphertextNumbers.push(num % 26);
+        }
+    }
+    return numberToLetter(ciphertextNumbers);
+}
+
+// Decodes a ciphertext encoded with a Hill Cypher knowing the encoding matrix
+std::string decode(const std::string ciphertext, const std::vector<std::vector<int>> encoding) {
+    std::queue<int> ciphertextNumbers = letterToNumber(ciphertext);
+    std::queue<int> plaintextNumbers;
+    // Throws exception if encoding matrix is not square (nxn) 
+    if (encoding.size() != encoding[0].size()) {
+        throw std::invalid_argument("Encoding Matrix is not square!");
+    }
+    // Ensures ciphertextNumbers is divisible by n by repeating the last element
+    int lastNum = ciphertextNumbers.back();
+    for (int i=0; i<ciphertextNumbers.size() % encoding.size(); i++) {
+        ciphertextNumbers.push(lastNum);
+    }
+    std::vector<std::vector<int>> inverse = invertMatrix(encoding);
+    // Continues until plaintextNumbers is empty
+    while (!ciphertextNumbers.empty()) {
+        std::vector<int> v; // Vector with length n
+        for (int i=0; i<inverse.size(); i++) { // Repeats n times
+            // Adds the front element of ciphertext to v and removes it from plaintextNumbers
+            v.push_back(ciphertextNumbers.front());
+            ciphertextNumbers.pop();
+        }
+        // Multiplies v with the encoding matrix and adds the result to ciphertextNumbers
+        v = multiplyMatrixVector(v, inverse);
+        for (int & num: v) {
+            plaintextNumbers.push(num % 26);
+        }
+    }
+    return numberToLetter(plaintextNumbers);
+}
+
+int main() {
+    std::string message = "this is a test message";
+    std::vector<std::vector<int>> key {{1, 2}, {3, 4}};
+    std::string encoded = encode(message, key);
+    std::string decoded = decode(encoded, key);
+    //std::cout<< encoded;
+    //std::cout << decoded;
+    std::vector<std::vector<int>> inverse = invertMatrix(key);
+    //std::vector<std::vector<int>> a = minor(key, 0, 0);
+    //std::cout << determinant(key);
+    for (int i=0; i < inverse.size(); i++) {
+        for (int j=0; j<inverse.size(); j++) {
+            std::cout << inverse[i][j] << " ";
+        }
+    }
+    return 0;
+}
